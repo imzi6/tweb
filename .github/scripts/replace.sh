b9b30d48d35b69bc4324e93a2f8af1e1b0619dcd
imzi6 <xireiki@foxmail.com>
2026-08-12 18:11:23 +0800

    Replace Telegram Backend API

--- .github/scripts/replace.sh
+++ .github/scripts/replace.sh
@@ -11,7 +11,7 @@ echo "HEAD Build: $LOCAL_BUILD"
 if [ "$UPSTREAM_BUILD" != "$LOCAL_BUILD" ]; then
 	git reset --hard "${UPSTREAM_BUILD}"
 	git cherry-pick "${LOCAL_BUILD}"..$(git log -2 --pretty=%H origin/master | tail -1)
-	find . \( -path './.git' -o -path './.github' \) -prune -o -type f -print0 | xargs -0 sed -i 's/\.web.telegram.org/.web.imzi.us.kg/g'
+	find . \( -path './.git' -o -path './.github' \) -prune -o -type f -print0 | xargs -0 sed -i 's/\.web.imzi.us.kg/.web.imzi.us.kg/g'
 	git add -A
 	git commit -m "Replace Telegram Backend API"
 	git push origin master --force-with-lease
